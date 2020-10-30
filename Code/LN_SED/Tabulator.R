60f02b86de0774a0699458d05f5af500520e99fe
Kaveh Siah <siahk@oregonstate.edu>
2020-10-29 23:10:56 -0700

    Started Auto-report code

--- Code/LN_SED/Tabulator.R
+++ Code/LN_SED/Tabulator.R
@@ -1,13 +1,11 @@
 #This Script Prepares Edited Data for direct report/manuscript calibration
-rm(list = ls())
 
-library(dpylr)
+library(dplyr)
 
-Cal_Pretable<-read.csv2("~/Project_Master/Test_Rep/Output_LN_SED/Edited Data/Soil_Solution_All2.csv", header=TRUE)
+Tab_Func_SS<-function(x){
+  x %>% group_by(group_id, YEAR) %>% summarise(across(Ca:HR, ~ mean(.x, na.rm = TRUE))) %>%
+  filter(group_id %in% c(10, 11, 12, 13, 14, 15, 16, 17))
+   
+  
+}
 
-Cal_Pretable 
-
-write.table(x, file = "", append = FALSE, quote = TRUE, sep = " ",
-            eol = "\n", na = "NA", dec = ".", row.names = TRUE,
-            col.names = TRUE, qmethod = c("escape", "double"),
-            fileEncoding = "")
