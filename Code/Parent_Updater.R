8d2a9980ac974da28186d8fc6747cb48712efc10
Coffee-Powered-Scientist <siahk@oregonstate.edu>
2021-03-01 19:32:22 -0800

    Although not complete, all harvest-site types have updated program and file outputs!

---
+++ Code/Parent_Updater.R
@@ -0,0 +1,26 @@
+
+# Master Updater
+
+source("~/Project_Master/Test_Rep/Code/LN_SED/Base/Updater.R")
+source("~/Project_Master/Test_Rep/Code/LN_SED/40_WTH/Updater.R")
+source("~/Project_Master/Test_Rep/Code/LN_SED/40_BO/Updater.R")
+source("~/Project_Master/Test_Rep/Code/LN_SED/80_WTH/Updater.R")
+source("~/Project_Master/Test_Rep/Code/LN_SED/80_BO/Updater.R")
+
+source("~/Project_Master/Test_Rep/Code/LN_BAS/Base/Updater.R")
+source("~/Project_Master/Test_Rep/Code/LN_BAS/40_WTH/Updater.R")
+source("~/Project_Master/Test_Rep/Code/LN_BAS/40_BO/Updater.R")
+source("~/Project_Master/Test_Rep/Code/LN_BAS/80_WTH/Updater.R")
+source("~/Project_Master/Test_Rep/Code/LN_BAS/80_BO/Updater.R")
+
+source("~/Project_Master/Test_Rep/Code/HN_BAS/Base/Updater.R")
+source("~/Project_Master/Test_Rep/Code/HN_BAS/40_WTH/Updater.R")
+source("~/Project_Master/Test_Rep/Code/HN_BAS/40_BO/Updater.R")
+source("~/Project_Master/Test_Rep/Code/HN_BAS/80_WTH/Updater.R")
+source("~/Project_Master/Test_Rep/Code/HN_BAS/80_BO/Updater.R")
+
+source("~/Project_Master/Test_Rep/Code/HN_SED/Base/Updater.R")
+source("~/Project_Master/Test_Rep/Code/HN_SED/40_WTH/Updater.R")
+source("~/Project_Master/Test_Rep/Code/HN_SED/40_BO/Updater.R")
+source("~/Project_Master/Test_Rep/Code/HN_SED/80_WTH/Updater.R")
+source("~/Project_Master/Test_Rep/Code/HN_SED/80_BO/Updater.R")
