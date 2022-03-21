ff12b086fab6006150258b4d3058514500e58419
Coffee-Powered-Scientist <siahk@oregonstate.edu>
2022-03-20 22:09:17 -0700

    Tested out figure generation code.

--- Code/Figure_Gen/40_BO/Agg_Biomass.R
+++ Code/Figure_Gen/40_BO/Agg_Biomass.R
@@ -401,7 +401,7 @@ BO_Aggregated_Biomass<-ggplot(Biomass_Master[order(Biomass_Master$variable,decre
   theme_bw()+
   theme(legend.background = element_rect(fill = "lightgray"), text=element_text(family="A", size=18), strip.text.x = element_text(size = 18),
         axis.text.x = element_text(size=12))+
-  geom_hline(data=Line , aes(yintercept = value, color=N), show.legend = FALSE)+
+  #geom_hline(data=Line , aes(yintercept = value, color=N), show.legend = FALSE)+
   #geom_hline(yintercept = 235, color="red")+
   scale_y_continuous(breaks=c(200, 400, 600, 800, 1000))+
   coord_cartesian(ylim = c(0, 1000))
