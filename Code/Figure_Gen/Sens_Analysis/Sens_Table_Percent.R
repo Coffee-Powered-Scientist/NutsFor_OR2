42b9806a0ede1206b4c3157234f87ed6584ebb8e
Coffee-Powered-Scientist <siahk@oregonstate.edu>
2022-05-17 09:44:31 -0700

    Updated sens. figures for Biogeochemistry manuscript.

---
+++ Code/Figure_Gen/Sens_Analysis/Sens_Table_Percent.R
@@ -0,0 +1,43 @@
+
+
+# Take percent sens. output and generates a pre-table to be used for the manuscript
+
+## Atmospheric Sens. Variable
+
+AtmBio<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/AtmBio_Per.csv')
+AtmLeach<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/AtmLeach_Per.csv')
+AtmWeath<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/AtmWeath_Per.csv')
+
+
+## Exchangeable Sens. Variable
+
+ExBio<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/ExBio_Per.csv')
+ExLeach<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/ExLeach_Per.csv')
+ExWeath<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/ExWeath_Per.csv')
+
+
+## Growth Sens. Variable
+
+GrwthBio<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/GrowthBio_Per.csv')
+GrwthLeach<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/LeachGrowth_Per.csv')
+GrwthWeath<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/GrowthWeath_Per.csv')
+
+
+## Mineral Sens. Variable
+
+MinBio<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/MinBio_Per.csv')
+MinLeach<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/MinLeach_Per.csv')
+MinWeath<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/MinWeath_Per.csv')
+
+## Nitrification
+
+NitBio<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/NitBio_Per.csv')
+NitLeach<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/NitLeach_Per.csv')
+NitWeath<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/NitWeath_Per.csv')
+
+
+## SOMP 
+
+NitBio<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/SOMPGrowth_Per.csv')
+NitLeach<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/SOMPLeach_Per.csv')
+NitWeath<-read.csv2('~/Project_Master/Test_Rep/Manuscript/Images/Sens_Analysis/SOMPWeath_Per.csv')
